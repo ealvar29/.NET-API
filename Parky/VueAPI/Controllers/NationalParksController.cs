82534592b29c6f4865e31049a3f866b8022cdcf6
Eduardo <ealvarez@carminaticonsulting.com>
2021-11-23 12:38:50 -0600

    add new nationalPark controller

---
+++ Parky/VueAPI/Controllers/NationalParksController.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using VueAPI.Repository.IRepository;
+
+namespace VueAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NationalParksController : Controller
+    {
+        private INationalParkReposity _npRepo;
+        private readonly IMapper _mapper;
+
+        public NationalParksController(INationalParkReposity npRepo, IMapper mapper)
+        {
+            _npRepo = npRepo;
+            _mapper = mapper;
+        }
+    }
+}
