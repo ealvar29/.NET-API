bb6ab447921e1722c7d016823e23252509d9904e
Eduardo <ealvarez@carminaticonsulting.com>
2022-04-09 15:31:11 -0500

    added repository and create method

---
+++ Parky/ParkyWeb/Repository/IRepository/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ParkyWeb.Repository.IRepository
+{
+    interface IRepository <T> where T : class
+    {
+        Task<T> GetAsync(string url, int Id);
+
+        Task<IEnumerable<T>> GetAllAsync(string url);
+
+        Task<bool> CreateAsync(string url, T objToCreate);
+
+        Task<bool> UpdateAsync(string url, T objToCreate);
+
+        Task<bool> DeleteAsync(string url, int Id);
+    }
+}
